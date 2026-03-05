296b03bfb325e5b345f203e61dc3c9c378503c45
Morten Prom <morten.prom@neptune-software.com>
2026-03-05 20:50:22 +0100

    Loading

--- artifacts/Application/prom_load_init_master/script/neptune.Script/Javascript.js
+++ artifacts/Application/prom_load_init_master/script/neptune.Script/Javascript.js
@@ -1,19 +1,19 @@
 
 sap.n.Shell.attachInit((data) => {
     console.log("prom_load_init_master", "Init", data);
-    AppCache.Load("prom_load_init_sync", {
-        load: "init",
-        parentObject: syncContainer,
-        // appGUID: "child_sync_app",
-        startParams: {
-            one: "One",
-            two: "Two",
-        }
-    });
-    AppCache.Load("prom_load_second_sync", {
-        load: "init",
-        parentObject: secondSync,
-    });
+    // AppCache.Load("prom_load_init_sync", {
+    //     load: "init",
+    //     parentObject: syncContainer,
+    //     // appGUID: "child_sync_app",
+    //     startParams: {
+    //         one: "One",
+    //         two: "Two",
+    //     }
+    // });
+    // AppCache.Load("prom_load_second_sync", {
+    //     load: "init",
+    //     parentObject: secondSync,
+    // });
     // AppCache.Load("prom_load_third_dia", {
     //     load: "init",
     //     dialogShow: true,
